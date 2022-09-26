77b73fcbd513c5206695fc84d0f33ad1c98dfc03
Chris <condreaychris@gmail.com>
2022-09-26 16:35:06 -0700

    spelling fixed

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MOGODB_URI || "mongodb://localhost/workout-tracker", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout-tracker", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
