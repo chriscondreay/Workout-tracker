288e619087eb393b630ad6175c934ece7a12b57d
Chris Condreay <condreaychris@gmail.com>
2025-10-27 14:55:33 -0700

    fixing helpers

--- public/api.js
+++ public/api.js
@@ -1,3 +1,4 @@
+// In api.js
 async function parseJsonOrThrow(res) {
     if (!res) throw new Error('No response (network error?)');
     if (!res.ok) {
@@ -13,46 +14,30 @@ async function parseJsonOrThrow(res) {
 }
 
 const API = {
-  async getLastWorkout() {
-    let res;
-    try {
-      res = await fetch("/api/workouts");
-    } catch (err) {
-      console.log(err)
-    }
-    const json = await res.json();
-
-    return json[json.length - 1];
-  },
-  async addExercise(data) {
-    const id = location.search.split("=")[1];
-
-    const res = await fetch("/api/workouts/" + id, {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(data)
-    });
-
-    const json = await res.json();
-
-    return json;
-  },
-  async createWorkout(data = {}) {
-    const res = await fetch("/api/workouts", {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: { "Content-Type": "application/json" }
-    });
-
-    const json = await res.json();
-
-    return json;
-  },
-
-  async getWorkoutsInRange() {
-    const res = await fetch(`/api/workouts/range`);
-    const json = await res.json();
-
-    return json;
-  },
+    async getLastWorkout() {
+        const res = await fetch("/api/workouts");
+        const json = await parseJsonOrThrow(res);
+        return json[json.length - 1];
+    },
+    async addExercise(data) {
+        const id = new URLSearchParams(location.search).get('id');
+        const res = await fetch(`/api/workouts/${id}`, {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(data)
+        });
+        return parseJsonOrThrow(res);
+    },
+    async createWorkout(data = {}) {
+        const res = await fetch("/api/workouts", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(data)
+        });
+        return parseJsonOrThrow(res);
+    },
+    async getWorkoutsInRange() {
+        const res = await fetch("/api/workouts/range");
+        return parseJsonOrThrow(res);
+    },
 };
