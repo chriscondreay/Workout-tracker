faf73dc99d2f533fbaf4b0b46645c71ced9f05db
Chris Condreay <condreaychris@gmail.com>
2025-10-28 13:58:24 -0700

    minor reset

--- public/index.js
+++ public/index.js
@@ -1,12 +1,13 @@
-// functions/index.js
-const functions = require("firebase-functions");
-const express = require("express");
-const app = express();
+init();
 
-app.get("/api/workouts", /* handler */);
-app.get("/api/workouts/range", /* handler */);
-app.post("/api/workouts", /* handler */);
-app.put("/api/workouts/:id", /* handler */);
-
-exports.api = functions.https.onRequest(app);
+async function init() {
+  if (location.search.split("=")[1] === undefined) {
+    const workout = await API.getLastWorkout();
+    if (workout) {
+      location.search = "?id=" + workout._id;
+    } else {
+      document.querySelector("#continue-btn").classList.add("d-none")
+    }
+  }
+}
 
